b41ed951c97335e7f66a1305ec494b390eed79b0
Jose Acevedo <jose.acevedo1@uac.edu.co>
2019-05-25 01:35:50 -0500

    lectura por habilidad

--- app/Http/Controllers/LecturaController.php
+++ app/Http/Controllers/LecturaController.php
@@ -7,19 +7,45 @@ use Illuminate\Http\Request;
 use App\Desarrollador;
 use App\Ocupacion;
 use App\Disponibilidad;
+use App\Habilidades;
 
 class LecturaController extends Controller
 {
     public function DesarrolladoresList(Request $request)
     {
-        $reclutador = DB::table('reclutadors')->count();
+        $reclutador = DB::table('reclutadors')->where('id_persona',$request->user()->id_persona)->count();
         if ($reclutador == 1) {
             $desarrollador = Desarrollador::with('hv_link','experiencia','habilidad','ocupacion','disponibilidad','persona')->get();
             return response()->json([
                 'User'=>$request->user(),
-
-               'Desarrollador'=> $desarrollador->all()
+                'Desarrollador'=> $desarrollador->all()
+            ]);
+        }else{
+            return response()->json([
+                'Response' => 'Access Denied',
+                'Description'=>'Is not a Reclutador',
+                'Code_error' => '401'
             ]);
         }
     }
+
+    public function DesarrolladoresListHabilidad(Request $request,$name)
+    {
+        $reclutador = DB::table('reclutadors')->where('id_persona',$request->user()->id_persona)->count();
+        if ($reclutador == 1) {
+            $habilidades = Habilidades::with('desarrollador')->where('Nombre_habilidad',$name)->get();
+            return response()->json([
+                'User'=>$request->user(),
+                'habilidades'=> $habilidades->all()
+            ]);
+        }else{
+            return response()->json([
+                'Response' => 'Access Denied',
+                'Description'=>'Is not a Reclutador',
+                'Code_error' => '401'
+            ]);
+        }
+    }
+
+
 }
--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::group(['middleware' => 'auth:api'], function () {
-    Route::get('profile', 'LecturaController@DesarrolladoresList');
+    Route::get('desarrollador_list', 'LecturaController@DesarrolladoresList');
+    Route::get('desarrollador_list/habilidad/{name}', 'LecturaController@DesarrolladoresListHabilidad');
 });
 
 
